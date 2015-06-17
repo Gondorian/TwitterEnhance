727e1bb78a7737f83427b0ebad6a6d30f1312220
Paul-Azevedo <Paul.Azevedo@uoit.net>
2015-06-17 15:14:35 -0400

    profilepage materialize update

---
+++ public/js/loginPage.js
@@ -0,0 +1,110 @@
+var Navbar = React.createClass({
+  render:function(){
+    return(
+      <div className="navbar">
+        <form className="languageChoice">
+        language:
+          <select>
+            <option value="English">English</option>
+            <option value="French">French</option>
+          </select>
+        </form>
+      </div>
+    );
+  }
+});
+
+var LoginBox =React.createClass({
+  render: function(){
+    return(
+      <div className="loginBox">
+        <form className ="form" action="http://localhost:3000/users/login" method="POST">
+          <input type="text" placeholder="Email" name="email"/><br/>
+          <input type="password" placeholder="Password" id="password" name="password"/>
+          <input type="submit" value="Log In" id="logButton" />
+          <input type="checkbox" value="remember" className="check" name="group" id="remember"/> 
+          <label htmlFor="remember">Remember me</label>
+          <a href="#">Forgot Password? </a>
+        </form>
+      </div>
+    );
+  }
+});
+
+var RegisterBox = React.createClass({
+  render: function() {
+    return (
+      <div className="registerBox">
+      <form id = "registration" className="form" action="http://localhost:3000/users/register" method="POST">
+      <h3><b>New to Video45?</b> Sign up</h3><hr />
+          <input type="text" placeholder="Full Name" name="fullName" /><br/>
+          <input type="text" placeholder="Email" name="email"/><br/>
+          <input type="text" placeholder="Username" name="username" /><br/>
+          <input type="password" placeholder="Password" id="pass" name="password"/><br/>
+          <input type="submit" value="Sign up for Video45" id="regButton"/>
+        </form>
+      </div>
+    );
+  }
+});
+
+var FrontPage = React.createClass({
+  render: function(){
+    return (
+      <div className="menus">
+        <Navbar />
+        <div className="content">
+          <LoginBox />
+          <RegisterBox />
+        </div>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <FrontPage />,
+  document.getElementById('register')
+);
+
+var toggleHelp = -1;
+
+var imageToggle = function(){
+  if(toggleHelp==1){
+    $('#wrapper').css('background-image','url("http://www.best-free-wallpaper.org/wp-content/uploads/2014/09/super-high-resolution-nature-wallpaper.jpg")');
+  }else{
+    $('#wrapper').css('background-image','url("http://cdn.wonderfulengineering.com/wp-content/uploads/2014/03/high-resolution-wallpapers-25.jpg")');
+  }
+  toggleHelp = toggleHelp*-1;
+  setTimeout(function(){
+      imageToggle();
+  },10000);
+}
+
+$(document).ready(function(){
+  var width = parseInt($('#wrapper').css('width'));
+  $('#registration').ajaxForm(function(response) { 
+    alert("response accepted"+response); 
+  });
+  if(width<860){
+    $('.textField').addClass('largeScreen');
+    $('#frontpage').css('margin-top','50px')
+  }else{
+    $('.textField').removeClass('largeScreen');
+    $('#frontpage').css('margin-top','100px')
+  }
+  imageToggle();
+});
+
+
+$(window).resize(function(){
+  var width = parseInt($('#wrapper').css('width'));
+  if(width<860){
+    $('.textField').addClass('largeScreen');
+    $('#frontpage').css('margin-top','50px')
+  }else{
+    $('.textField').removeClass('largeScreen');
+    $('#frontpage').css('margin-top','100px')
+  }
+  console.log(width);
+});
